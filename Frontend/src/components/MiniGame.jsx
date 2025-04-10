4c2d759aaf804bd6b4e8922a7df322c9e4788063
413f10r <ale-fioretti@live.it>
2025-04-10 04:25:24 +0200

    add: create MiniGame component with game container and styles

--- Frontend/src/components/MiniGame.jsx
+++ Frontend/src/components/MiniGame.jsx
@@ -1,4 +1,5 @@
 import portaCalcio from "../assets/img/category/portaCalcio.jpg"; // Importa l'immagine della porta calcio
+import "../css/index.css"; // Importa correttamente il file CSS
 
 export default function MiniGame() {
     return (
@@ -7,6 +8,13 @@ export default function MiniGame() {
             <figure style={{ width: "98vw", margin: 0 }}>
                 <img src={portaCalcio} alt="Porta da Calcio" style={{ width: "100%", display: "block" }} />
             </figure>
+
+            <div className="game-container">
+                <h3>Gioca ora</h3>
+                <div className="goal">
+                    <div className="ball"></div>
+                </div>
+            </div>
         </>
     );
 }
